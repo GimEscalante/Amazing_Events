046e9b7f941b27974c54fd1aff5773d7c194ee20
Gimena Escalante <gimeescalante1@gmail.com>
2023-03-08 01:04:19 -0300

    task2: usando js

---
+++ folder/past_events.js
@@ -0,0 +1,22 @@
+let cardmain = document.getElementById("tarjeta-dinamica")
+console.log(cardmain)
+
+let stringHtml = ""
+
+for (events of data.events) {
+    if (events.date <= data.currentDate) {
+        console.table(events)
+        stringHtml += `<div class="card" style="width: 25rem; height:32rem;">
+        <img src="${events.image}" class="card-img-top p-3" alt="...">
+        <div class="card-body text-center">
+        <h5 class="card-title text-center">${events.name}</h5>
+        <p class="card-text text-center">${events.description}</p>
+        <p class="text-start">${`Price $` + events.price}</p>
+        <a href="#" class="btn btn-primary">Ver más</a>
+        </div>
+        </div>`
+    }
+
+}
+
+cardmain.innerHTML=stringHtml
